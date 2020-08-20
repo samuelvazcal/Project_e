1ce3150f49050b5ea50d72fafe3fd1d2457a5b7b
Samuel Vazquez <samuel_vazquez@epam.com>
2020-08-20 14:41:42 -0500

    Cleaning useless variables

--- Problem-2/src/Main.java
+++ Problem-2/src/Main.java
@@ -2,9 +2,8 @@ public class Main {
 	public static void main(String[] args) {
 		int firstNum = 0;
 		int secondNum = 1;
-		int count=0;
+		int count;
 		int sum = 0;
-		int number = 1;
 		for(int i=1; sum < 4000000;i++){
 			count = firstNum+secondNum;
 			firstNum=secondNum;

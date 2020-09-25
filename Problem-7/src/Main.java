2004db1f1f7aee4e4a3f801f90177105f25d4c42
Samuel Vazquez <samuel_vazquez@epam.com>
2020-09-25 16:32:23 -0500

    Finding the XXX st prime number using static methods and flow control statements

---
+++ Problem-7/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		final int MAX_VALUE = Integer.MAX_VALUE;
+		final int LIMIT = 10001;
+
+		System.out.println("The " + LIMIT + "st prime number is : " + OrdinalPrimeNumber.primeNumber(MAX_VALUE,LIMIT));
+
+
+	}
+
+
+}
---
+++ Problem-7/src/OrdinalPrimeNumber.java
@@ -0,0 +1,30 @@
+public class OrdinalPrimeNumber {
+
+	public static int primeNumber (int maxValue, int limit) {
+		int count = 0;
+		int i = 1;
+		for(; i < maxValue; i++) {
+			if(isPrime(i)) {
+				count++;
+				//System.out.println("Number " + i + " is a prime number");
+				if(count == limit) {
+					break;
+				}
+			}
+		}
+		return i;
+	}
+
+	public static boolean isPrime(int n) {
+		if(n==1) {
+			return false;
+		}
+		for(int i = 2; i<=n/2; i++) {
+			if(n%i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
